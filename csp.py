faf27f0d3f21759fae7459494d39b4b3a15c441f
dzee <df.zee@hotmail.com>
2023-03-23 17:00:05 +0100

    unit tests passed

--- csp.py
+++ csp.py
@@ -6,9 +6,8 @@ import typing
 
 
 class CSP:
-    def __init__(self, grid:np.ndarray, numbers: typing.Set[int], groups: typing.List[typing.List[typing.Tuple[int,int]]],
-                 constraints: typing.List[typing.Tuple[int,int]]):
-        
+    def __init__(self, grid: np.ndarray, numbers: typing.Set[int], groups: typing.List[typing.List[typing.Tuple[int, int]]],
+                 constraints: typing.List[typing.Tuple[int, int]]):
         """
         The CSP solver object, containing all information and functions required for the assignment. You do not need to change
         this function.
@@ -17,7 +16,7 @@ class CSP:
         :param numbers: The set of numbers that we are allowed to use in order to fill the grid (can be any set of integers)
         :param groups: A list of cell groups (cell groups are lists of location tuples).
         :param constraints: The list of constraints for every group of cells. constraints[i] hold for groups[i]. Every
-                            constraint is a tuple of the form (sum_of_elements, max_count_element) where sum_of_elements 
+                            constraint is a tuple of the form (sum_of_elements, max_count_element) where sum_of_elements
                             indicates what the sum must be of the elements of the given group, and max_count_element indicates
                             the maximum number of times that a number/element may occur in the given group
         """
@@ -31,97 +30,103 @@ class CSP:
         self.grid = grid
         self.cell_to_groups = {(row_idx, col_idx): [] for row_idx in range(self.height) for col_idx in range(self.width)}
 
-
     def fill_cell_to_groups(self):
         """
         Function that fills in the self.cell_to_groups datastructure, which maps a cell location (row_idx, col_idx)
         to a list of groups of which it is a member. For example, suppose that cell (0,0) is member of groups 0, 1,
-        and 2. Then, self.cell_to_groups[(0,0)] should be equal to [0,1,2]. This function should do this for every cell. 
-        If a cell is not a member of any groups, self.cell_to_groups[cell] should be an empty list []. 
-        The function does not return anything. 
+        and 2. Then, self.cell_to_groups[(0,0)] should be equal to [0,1,2]. This function should do this for every cell.
+        If a cell is not a member of any groups, self.cell_to_groups[cell] should be an empty list [].
+        The function does not return anything.
 
         Before completing this function, make sure to read the assignment description and study the data structures created
         in the __init__ function above (self.groups and self.cell_to_groups).
         """
 
-        # TODO: write this function 
+        for cell in self.cell_to_groups:
+            self.cell_to_groups[cell] = [group_num for group_num, group in enumerate(self.groups) if cell in group]
 
-        raise NotImplementedError()
-
-
-    def satisfies_sum_constraint(self, group: typing.List[typing.Tuple[int,int]], sum_constraint: int) -> bool:
+    def satisfies_sum_constraint(self, group: typing.List[typing.Tuple[int, int]], sum_constraint: int) -> bool:
         """
-        Function that checks whether the given group satisfies the given sum constraint (group smaller or equal 
-        than sum). Returns True if the current group satisfies the constraint and False otherwise. 
+        Function that checks whether the given group satisfies the given sum constraint (group smaller or equal
+        than sum). Returns True if the current group satisfies the constraint and False otherwise.
 
-        :param group: The list of locations [loc1, loc2, loc3,...,locN] that specify the group. Here, every loc is 
-                      a tuple (row_idx, col_idx) of indices, specifying the row and column of the cell. 
+        :param group: The list of locations [loc1, loc2, loc3,...,locN] that specify the group. Here, every loc is
+                      a tuple (row_idx, col_idx) of indices, specifying the row and column of the cell.
         :param sum_constraint: The sum_of_elements constraint specifying that the numbers in the given group must
-                               sum up to this number. This is None if there is no sum constraint for the given group. 
+                               sum up to this number. This is None if there is no sum constraint for the given group.
         """
 
-        # TODO: write this function
+        if sum(self.grid[row_idx, col_idx] for row_idx, col_idx in group) <= sum_constraint:
+            return True
+        else:
+            return False
 
-        raise NotImplementedError()
-
-    
-    def satisfies_count_constraint(self, group: typing.List[typing.Tuple[int,int]], count_constraint: int) -> bool:
+    def satisfies_count_constraint(self, group: typing.List[typing.Tuple[int, int]], count_constraint: int) -> bool:
         """
         Function that checks whether the given group satisfies the given count constraint.
-        Returns True if the current group satisfies the constraint and False otherwise. 
+        Returns True if the current group satisfies the constraint and False otherwise.
         Recall that the value of 0 indicates an empty cell (0s should not count towards the count constraint).
 
-        :param group: The list of locations [loc1, loc2, loc3,...,locN] that specify the group. Here, every loc is 
-                      a tuple (row_idx, col_idx) of indices, specifying the row and column of the cell. 
-        :param count_constraint: Integer specifying that a given number cannot occur more than this amount of times. 
-                                 This is None if there is no count constraint for the given group. 
+        :param group: The list of locations [loc1, loc2, loc3,...,locN] that specify the group. Here, every loc is
+                      a tuple (row_idx, col_idx) of indices, specifying the row and column of the cell.
+        :param count_constraint: Integer specifying that a given number cannot occur more than this amount of times.
+                                 This is None if there is no count constraint for the given group.
         """
 
-        # TODO: write this function
-
-        raise NotImplementedError()
-
+        cell_values, cell_counts = np.unique([self.grid[row_idx, col_idx] for row_idx, col_idx in group], return_counts=True)
+        for value, count in zip(cell_values, cell_counts):
+            if value != 0 and count > count_constraint:
+                return False
+        else:
+            return True
 
     def satisfies_group_constraints(self, group_indices: typing.List[int]) -> bool:
         """
         Function that checks whether the constraints for the given group indices are satisfied.
-        Returns True if all relevant constraints are satisfied, False otherwise. Make sure to use functions defined above. 
+        Returns True if all relevant constraints are satisfied, False otherwise. Make sure to use functions defined above.
 
-        :param group_indices: The indices of the groups for which we check all of the constraints 
+        :param group_indices: The indices of the groups for which we check all of the constraints
         """
 
-        # TODO: write this function
-
-        raise NotImplementedError()
-
+        for index in group_indices:
+            if not (self.satisfies_sum_constraint(self.groups[index], self.constraints[index][0]) and
+                    self.satisfies_count_constraint(self.groups[index], self.constraints[index][1])):
+                return False
+        else:
+            return True
 
     def search(self, empty_locations: typing.List[typing.Tuple[int, int]]) -> np.ndarray:
         """
         Recursive exhaustive search function. It tries to fill in the empty_locations with permissible values
         in an attempt to find a valid solution that does not violate any of the constraints. Instead of checking all
-        possible constraints after filling in a number, it checks only the relevant group constraints using the 
-        self.cell_to_groups data structure. 
+        possible constraints after filling in a number, it checks only the relevant group constraints using the
+        self.cell_to_groups data structure.
 
         Returns None if there is no solution. Returns the filled in solution (self.grid) otherwise if a solution is found.
 
-        :param empty_locations: list of empty locations that still need a value from self.numbers 
+        :param empty_locations: list of empty locations that still need a value from self.numbers
         """
 
-        # TODO: write this function
-
-        raise NotImplementedError()
-    
+        if len(empty_locations) > 0:
+            for number in self.numbers:
+                self.grid[empty_locations[0]] = number
+                grid = self.search(empty_locations[1:])
+                if grid is not None and self.satisfies_group_constraints(self.cell_to_groups[empty_locations[0]]):
+                    return grid
+            else:
+                return None
+        else:
+            return self.grid
 
     def start_search(self):
         """
         Non-recursive function that starts the recursive search function above. It first fills the cell_to_group
-        data structure and computes the empty locations. Then, it starts the recursive search procedure. 
+        data structure and computes the empty locations. Then, it starts the recursive search procedure.
         The result is None if there is no solution possible. Otherwise, it returns the grid that is a solution.
 
         You do not need to change this function.
         """
 
         self.fill_cell_to_groups()
-        empty_locations = [(row_idx, col_idx) for row_idx in range(self.height) for col_idx in range(self.width) if self.grid[row_idx,col_idx]==0]
+        empty_locations = [(row_idx, col_idx) for row_idx in range(self.height) for col_idx in range(self.width) if self.grid[row_idx, col_idx] == 0]
         return self.search(empty_locations)
-    
